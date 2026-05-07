8db99afb0342365fbd5d9f696d43a66e9cfa4ef3
arlanklevyn-criador <arlanklevyn@gmail.com>
2026-05-07 09:31:04 -0300

    mensagem do commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@300;400;500&display=swap');
 
